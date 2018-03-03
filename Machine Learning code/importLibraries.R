30d048524109bde9fd3eb7ccfaeaadb6fc3bf146
Adam Calhoun <a.t.calhoun@gmail.com>
2018-03-02 16:48:06 -0800

    R code now checks for missing data in both the Age and Salary columns

--- Machine Learning code/importLibraries.R
+++ Machine Learning code/importLibraries.R
@@ -4,3 +4,7 @@ dataset$Age = ifelse(is.na(dataset$Age),
                    ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),# average, built into R
                    dataset$Age)
 # R syntax ifelse (condition, ifTrue, else)
+
+dataset$Salary = ifelse(is.na(dataset$Salary),
+                     ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),# average, built into R
+                     dataset$Salary)
